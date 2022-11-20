3ad7721ec198673c053e26baec9bfaac24673b12
uenomanami <xi2.ut0626@gmail.com>
2022-11-20 16:56:48 +0900

    second-commit

--- app/Http/Requests/TodoRequest.php
+++ app/Http/Requests/TodoRequest.php
@@ -24,7 +24,7 @@ class TodoRequest extends FormRequest
     public function rules()
     {
         return [
-            'content' => 'required',
+            'content' => 'required|string|max:20',
             //
         ];
     }
